4020159db8f156fb9cf91a2abd0953c2d9e2df61
Charlie Mudd <cmudd6@gmail.com>
2023-03-13 22:58:03 -0700

    main page first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const columnContainer = document.getElementById('column-container')
+
+const getResults = async() => {
+    const data = await fetch('https://www.reddit.com/r/popular.json')
+    const jsonData = await data.json()
+    const posts = jsonData.data.children
+   
+    
+    posts.forEach(post => {
+        let content = post.data
+        let newDiv = document.createElement('div')
+        newDiv.classList.add('post-window')
+
+        newDiv.classList.add('post-window');
+        document.body.appendChild(newDiv)
+
+        const headerDiv = document.createElement('div')
+        headerDiv.classList.add('header-div')
+        newDiv.append(headerDiv)
+
+
+     //FOR SUBREDDIT NAME
+
+        const subReddit = document.createElement('h4')
+        subReddit.classList.add('subreddit')
+        subReddit.innerHTML = content.subreddit
+        headerDiv.appendChild(subReddit)
+
+    // FOR THUMBNAILS
+
+        if(content.thumbnail != 'self' && content.thumbnail != "" && content.thumbnail != "nsfw" && content.thumbnail != "default" && content.thumbnail != "image"){
+            const thumbNail = document.createElement('img')
+            thumbNail.classList.add('thumbnail')
+            thumbNail.src = content.thumbnail
+            headerDiv.appendChild(thumbNail)
+
+            
+        } else if(content.thumbnail == 'nsfw') {
+            const thumbNail = document.createElement('img')
+            thumbNail.classList.add('thumbnail')
+            thumbNail.src = './nsfw.jpeg'
+            headerDiv.appendChild(thumbNail) 
+           
+        }
+    
+    
+    //FOR TITLE
+
+
+        let header = document.createElement('h3')
+        header.classList.add('header')
+        header.innerHTML = content.title
+        newDiv.appendChild(header)
+    
+    //FOR URL
+        if(content.url && content.link_flair_type == 'text'){
+        let url = document.createElement('p')
+        url.innerHTML = content.url
+        url.classList.add('url')
+        newDiv.append(url)
+        }
+    //FOR IMAGES AND VIDEO
+
+      
+        if(content.secure_media_embed && content.secure_media_embed.media_domain_url){
+            let videoContent = document.createElement('iframe')
+            videoContent.src = content.secure_media_embed.media_domain_url
+           videoContent.classList.add('movies')
+            newDiv.appendChild(videoContent)
+       
+            
+
+        } else if(content.media && content.media.reddit_video && content.media.reddit_video.fallback_url){
+            let videoContent = document.createElement('video')
+            videoContent.src = content.media.reddit_video.fallback_url
+            videoContent.controls = true;
+            videoContent.classList.add('movies')
+            newDiv.appendChild(videoContent)
+          
+
+        } else if(content.crosspost_parent_list && content.crosspost_parent_list[0].secure_media && content.crosspost_parent_list[0].secure_media.reddit_video && content.crosspost_parent_list[0].secure_media.reddit_video.fallback_url){
+            let videoContent = document.createElement('video')
+            videoContent.src = content.crosspost_parent_list[0].secure_media.reddit_video.fallback_url
+            videoContent.controls = true;
+            videoContent.classList.add('movies')
+            newDiv.appendChild(videoContent)
+        
+        } else if(content.is_reddit_media_domain == true){
+            let image = document.createElement('img')
+            image.src = content.url_overridden_by_dest
+            image.classList.add('image')
+            newDiv.appendChild(image)
+        } 
+
+    //FOR GETTING TEXT IN THE ELEMENT
+    if(content.selftext){
+        let text = document.createElement('p')
+        text.textContent = content.selftext
+        // console.log(text)
+        const textDiv = document.createElement('div')
+        textDiv.classList.add('text-div')
+        newDiv.append(textDiv)
+        text.classList.add('text')
+        textDiv.append(text)
+        // console.log(text.innerHTML.length)
+}
+
+
+
+    })
+
+
+}
+
+getResults()
